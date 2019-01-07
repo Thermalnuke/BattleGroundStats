ec08f85bd1af327e34c5a91dbf38be0e82c29f4a
Patrick Thompson <thompsonpa2014@gmail.com>
2019-01-06 23:22:22 -0500

    Update Main.lua
    set slash command, no longer loads on startup /bgs is the command

--- OldRick/Main.lua
+++ OldRick/Main.lua
@@ -1,16 +1,24 @@
 local bgsPlayed = ""
 local bgsWon = ""
-local UIConfig = CreateFrame("Frame",OLDDICK_MainFrame, UIParent, "BasicFrameTemplateWithInset")
-UIConfig:SetSize(400,360);
-UIConfig:SetPoint("CENTER",UIParent,"CENTER");
 
---Child Frame Widgets
-UIConfig.title = UIConfig:CreateFontString(nil, "OLDDICK");
-UIConfig.title:SetFontObject("GameFontHighlight");
-UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT",5,0);
-UIConfig.title:SetText("OLDDICK's Dick");
+SLASH_TEST1 = "/bgs"
 
-for _, CategoryId in pairs(GetStatisticsCategoryList()) do	
+SlashCmdList["TEST"] = function(msg)
+   Getinfo();
+end 
+
+function Getinfo()
+     local UIConfig = CreateFrame("Frame",OLDDICK_MainFrame, UIParent, "BasicFrameTemplateWithInset")
+     UIConfig:SetSize(400,360);
+     UIConfig:SetPoint("CENTER",UIParent,"CENTER");
+
+     --Child Frame Widgets
+     UIConfig.title = UIConfig:CreateFontString(nil, "OLDDICK");
+     UIConfig.title:SetFontObject("GameFontHighlight");
+     UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT",5,0);
+     UIConfig.title:SetText("OLDDICK's Dick");
+
+     for _, CategoryId in pairs(GetStatisticsCategoryList()) do	
 		for i = 1, GetCategoryNumAchievements(CategoryId) do
 			local ID, Name = GetAchievementInfo(CategoryId, i)
 			if Name == "Battlegrounds played" then
@@ -22,34 +30,37 @@ for _, CategoryId in pairs(GetStatisticsCategoryList()) do
 		end		
 	end
 
-function round(num, numDecimalPlaces)
-  local mult = 10^(numDecimalPlaces or 0)
-  return math.floor(num * mult + 0.5) / mult
-end
+    function round(num, numDecimalPlaces)
+         local mult = 10^(numDecimalPlaces or 0)
+         return math.floor(num * mult + 0.5) / mult
+    end
 	
-local bgsPlayedTitle = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightSmall")
-bgsPlayedTitle:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 10, -30)
-bgsPlayedTitle:SetText("Battlegrounds Played")
+     local bgsPlayedTitle = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightSmall")
+     bgsPlayedTitle:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 10, -30)
+     bgsPlayedTitle:SetText("Battlegrounds Played")
 
-local bgsWonTitle = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightSmall")
-bgsWonTitle:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 150, -30)
-bgsWonTitle:SetText("Battlegrounds Won")
+     local bgsWonTitle = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightSmall")
+     bgsWonTitle:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 150, -30)
+     bgsWonTitle:SetText("Battlegrounds Won")
 
-local bgsPercentTitle = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightSmall")
-bgsPercentTitle:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 280, -30)
-bgsPercentTitle:SetText("Win Percentage")
+     local bgsPercentTitle = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightSmall")
+     bgsPercentTitle:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 280, -30)
+     bgsPercentTitle:SetText("Win Percentage")
 
-local bgsPlayedNum = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightLarge")
-bgsPlayedNum:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 50, -50)
-bgsPlayedNum:SetText(bgsPlayed)
+     local bgsPlayedNum = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightLarge")
+     bgsPlayedNum:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 50, -50)
+     bgsPlayedNum:SetText(bgsPlayed)
 
-local bgsWonNum = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightLarge")
-bgsWonNum:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 190, -50)
-bgsWonNum:SetText(bgsWon)
+     local bgsWonNum = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightLarge")
+     bgsWonNum:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 190, -50)
+     bgsWonNum:SetText(bgsWon)
 
-local bgsPercentNum = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightLarge")
-bgsPercentNum:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 290, -50)
-bgsPercentNum:SetText(round((bgsWon / bgsPlayed)* 100,3) .. "%")
+     local bgsPercentNum = UIConfig:CreateFontString(nil, "ARTWORK", "GameFontHighLightLarge")
+     bgsPercentNum:SetPoint("TOPLEFT", UIConfig, "TOPLEFT", 290, -50)
+     bgsPercentNum:SetText(round((bgsWon / bgsPlayed)* 100,2) .. "%")
+end
+
+
 
 
 
